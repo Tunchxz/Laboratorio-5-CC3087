11be300642717b4a092c46e7d3958747b9c5518e
Sebas Tunchez <tun231359@uvg.edu.gt>
2024-08-28 21:33:39 -0600

    Codelab 3

---
+++ Codelab3/app/src/main/java/uvg/edu/codelab3/MainActivity.kt
@@ -0,0 +1,29 @@
+package uvg.edu.codelab3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import uvg.edu.codelab3.ui.theme.Codelab3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Codelab3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
